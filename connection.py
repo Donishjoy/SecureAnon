f647bcbed9a0afe4513c92155b045bd628890ef6
Donishjoy <114819415+Donishjoy@users.noreply.github.com>
2024-03-21 13:14:35 +0530

    Delete connection.py

--- connection.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-# Connect to MongoDB (replace with your connection details)
-client = pymongo.MongoClient("mongodb://localhost:27017/")  # Replace with your MongoDB connection string
-db = client["face"]  # Replace with your database name
-frames_collection = db["frames"]  # Replace with your collection nameex
-
